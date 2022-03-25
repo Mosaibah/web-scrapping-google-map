1990f5d6cb372bca0b2e20689e0311c36f7d2739
Abdulrahman <abudrm2020@gmail.com>
2022-03-25 12:41:29 +0300

    Fix bug

--- script.py
+++ script.py
@@ -4,26 +4,23 @@ from serpapi import GoogleSearch
 from urllib.parse import urlsplit, parse_qsl
 import pandas as pd
 
+
 def case_law_results():
+    word_term = input("Please enter word term to search on Google Map")
+    address = input("Please enter the address")
 
     print("Extracting case law results..")
 
     params = {
         "engine": "google_maps",
-        "q": "ملاعب",
+        "q": word_term,
         "type": "search",
         "start": "0",
-        "ll": "@26.4274657,50.1112348,14z",
+        "ll": f"@{address}",
         "api_key": "595a89814a7aec5c3bdb1c42d6fffdffc90ab61a6eb852f364e6f866531abce9"
     }
-    # params = {
-    #   "q": "ملاعب",
-    #   "location": "Ad Dammam, Eastern Province, Saudi Arabia",
-    #   "hl": "ar",
-    #   "gl": "sa",
-    #   "google_domain": "google.com.sa",
-    #   "api_key": "595a89814a7aec5c3bdb1c42d6fffdffc90ab61a6eb852f364e6f866531abce9"
-    # }
+ 
+ 
     search = GoogleSearch(params)
 
     results_data = []
@@ -32,32 +29,26 @@ def case_law_results():
     while loop_is_true:
       results = search.get_dict()
 
-    #   print(results['serpapi_pagination']['current'])
-    #   print(f"Currently extracting page №{results['serpapi_pagination']}..")
-    #   print(results)
 
-      for result in results["local_results"]:
-        title = result["title"]
-        result_type = result.get("type")
-
-        try:
-          phone = result["resources"][0]["title"]
-        except: phone = None
-
-        results_data.append({
-          "position": result["position"] + 1,
-          "result_type": result_type,
-          "title": title,
-          "phone": phone
-        })
-        # if i != 100 :
-        #   i += 1
-        #   search.params_dict.update(dict(parse_qsl(urlsplit(results["serpapi_pagination"]["next"]).query)))
-        # else:
-        #     loop_is_true = False
+      if "local_results" in results:
+        for result in results["local_results"]:
+          title = result["title"]
+          result_type = result.get("type")
+          try:
+            phone = result["phone"]
+          except: 
+            phone = None
+          results_data.append({
+            "result_type": result_type,
+            "title": title,
+            "phone": phone
+          })
+          
         if "next" in results["serpapi_pagination"]:
           search.params_dict.update(dict(parse_qsl(urlsplit(results["serpapi_pagination"]["next"]).query)))
         else:
+          loop_is_true = False
+      else: 
           loop_is_true = False
 
     return results_data
@@ -65,7 +56,7 @@ def case_law_results():
 
 def save_case_law_results_to_csv():
     print("Waiting for case law results to save..")
-    pd.DataFrame(data=case_law_results()).to_csv("google_scholar_case_law_results.csv", encoding="utf-8", index=False)
+    pd.DataFrame(data=case_law_results()).to_csv("google_scholar_case_law_results.csv", encoding="utf-8-sig", index=False)
 
     print("Case Law Results Saved.")
 
